c684bca6a3a56485ec3ab7d490841ab1f066cb0a
tiansimeng <2301547597@qq.com>
2018-07-07 19:33:18 +0800

    1111

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my frist project ");
+		
+		
+		System.out.println("t´ò·¨ÊÇ·ñ");
 	}
 
 }
