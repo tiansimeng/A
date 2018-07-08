99807ba26b19a97b4ef893c6c7292595f2817af2
tiansimeng <40911903+tiansimeng@users.noreply.github.com>
2018-07-08 10:12:33 +0800

    Update JJJnmm.java

--- A/src/JJJnmm.java
+++ A/src/JJJnmm.java
@@ -1,11 +1,13 @@
 
 
+
 public class JJJnmm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String type1 = "啊啊啊啊啊啊啊啊啊";
-		String type = "YE3MP-";
+		
+		String type = "YE3MP凄凄切切群群群-";
 	}
 
 }
