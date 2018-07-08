0268f71aba7f1c9eaa0ef17f04f56955b18bf0bd
tiansimeng <40911903+tiansimeng@users.noreply.github.com>
2018-07-08 10:56:52 +0800

    Update JJJnmm.java

--- A/src/JJJnmm.java
+++ A/src/JJJnmm.java
@@ -5,7 +5,7 @@ public class JJJnmm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String type1 = "啊啊啊啊啊啊啊啊啊";
+		String type1 = "啊啊啊啊啊啊啊啊啊1111111";
 		
 		String type = "YE3MP凄凄切切群群群-";
 		System.out.println(111);
